176a4a44463fc79fb2df37bb8fb72b51840afffa
LG29878 <grajalesliliana@johndeere.com>
2021-02-17 22:23:41 -0500

    stopwatch, increases and resets, pause does not pause

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -2,18 +2,44 @@ import React, { Component } from 'react';
 import "./Stopwatch.css"
 
 class Stopwatch extends Component {
-  render() {
+  constructor(){
+    super()
+     this.state ={
+      counter: 0,
+    }
+  }
+  pauseCount =()=>{
+   let counter=this.state.counter ;
+   this.setState({
+     counter
+    });
+ }
+ resetCount =()=>{
+  let counter=0 ;
+  this.setState({
+    counter
+  });
+}
+  interval=()=>{setInterval(() => {
+    console.log(this.counter)
+    let counter =this.state.counter + 1
+    this.setState({
+      counter
+      })
+     }, 1000);
+  }
+  render(){
     return (
       <div className="stopwatch">
-        <h1>0</h1>
+        <h1>{this.state.counter}  </h1>
         <div className="controls">
-          <button>Reset</button>
-          <button>Start</button>
-          <button>Pause</button>
+          <button onClick={this.resetCount}> Reset </button>
+          <button onClick={this.interval} >Start</button>
+          <button onClick={this.pauseCount}>Pause</button>
         </div>
       </div>
     );
   }
 }
-
 export default Stopwatch;
+
